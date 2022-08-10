6d8ce9849c7790b6efc7710e809f4b3982f2ee1e
Craig Stephenson <crstephenson@alaska.edu>
2022-08-10 15:55:37 -0800

    Map is populated with fish groups from REST API and report page is partially complete.

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <FisheriesMap v-if="!this.reportIsVisible" />
+  <FisheriesReport v-if="this.reportIsVisible" />
+</template>
+
+<script>
+import FisheriesMap from './components/FisheriesMap.vue'
+import FisheriesReport from './components/FisheriesReport.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'App',
+  components: {
+    FisheriesMap,
+    FisheriesReport
+  },
+  created() {
+    this.$store.dispatch("fetchRegions");
+    this.$store.dispatch("fetchFisheries");
+  },
+  computed: {
+    ...mapGetters({
+      reportIsVisible: 'reportIsVisible',
+    }),
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+body {
+  margin: 0;
+}
+.container:not(.is-max-desktop):not(.is-max-widescreen) {
+  max-width: 100% !important;
+  width: 100% !important;
+  padding: 0;
+}
+</style>
---
+++ src/components/FisheriesReport.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="report">
+    <h1 class="title is-1">{{ selectedGroup }} in {{ selectedRegion }}</h1>
+    <div id="report" v-if="groupedFisheries[selectedRegion] != undefined">
+    <div v-for="fishery in groupedFisheries[selectedRegion][selectedGroup]" :key="fishery">
+        <h3 class="title is-3" v-html="fishery['name']"></h3>
+        <table>
+        <tr>
+            <td style="width: 200px;">Access</td>
+            <td v-html="fishery['entry']"></td>
+        </tr>
+        <tr>
+            <td>Species</td>
+            <td v-html="fishery['species']"></td>
+        </tr>
+        <tr>
+            <td>Gear</td>
+            <td v-html="fishery['gear']"></td>
+        </tr>
+        <tr>
+            <td>Region</td>
+            <td v-html="fishery['region']"></td>
+        </tr>
+        <tr>
+            <td>CFEC code</td>
+            <td v-html="fishery['code']"></td>
+        </tr>
+        <tr>
+            <td>Seasons</td>
+            <td v-html="fishery['seasons']"></td>
+        </tr>
+        <tr>
+            <td>Link</td>
+            <td>
+            <a :href="fishery['link']">{{ fishery["link"] }}</a>
+            </td>
+        </tr>
+        </table>
+        <hr />
+    </div>
+    </div>
+    <div><button @click="reset">Back to map</button></div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.report {
+    margin: 0 2em;
+}
+h1, h3 {
+    text-align: left;
+}
+hr {
+    margin: 40px 0;
+}
+td {
+    text-align: left;
+}
+</style>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "FisheriesReport",
+  computed: {
+    ...mapGetters({
+      groupedFisheries: "groupedFisheries",
+      selectedRegion: "selectedRegion",
+      selectedGroup: "selectedGroup"
+    }),
+  },
+  methods: {
+    reset: function () {
+      this.$store.commit("closeReport");
+    },
+  },
+};
+</script>
